d447a59e14ec20edf839c1b17d0f2e7f1dbebefe
James Dearing <dearingj@lifetime.oregonstate.edu>
2016-12-13 01:59:25 -0800

    Added dummy tests

---
+++ app/src/test/java/anupam/acrylic/PictureLayoutTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2014, 2016 Valerio Bozzolan & James Dearing (TheOpenSourceNinja)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package anupam.acrylic;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PictureLayoutTest {
+	@Before
+	public void setUp( ) throws Exception {
+		
+	}
+	
+	@After
+	public void tearDown( ) throws Exception {
+		
+	}
+	
+	@Test
+	public void addView( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void addView1( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void addView2( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void addView3( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void generateDefaultLayoutParams( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void onMeasure( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void dispatchDraw( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void invalidateChildInParent( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+	@Test
+	public void onLayout( ) throws Exception {
+		//Just creating some dummy tests for now. They will always fail. Replace this with a real test:
+		assertFalse( "Test not implemented", true );
+	}
+	
+}
